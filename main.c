3f006ddb02f7bac3886c42b9dd95e7e942da04c4
axododo <guilbertmael@gmail.com>
2026-07-26 00:07:10 +0200

    fix bassique

--- main.c
+++ main.c
@@ -1,52 +1,50 @@
 #include "philo.h"
 
-
 int check_args(char **av) {
   int i;
   int y;
 
   i = 0;
   y = 0;
-  while (av[y]){
+  while (av[y]) {
     while (av[y][i]) {
-        if (av[y][i] < '0' || av[y][i] > '9')
-          return(1);
+      if (av[y][i] < '0' || av[y][i] > '9')
+        return (1);
       i++;
     }
     y++;
   }
-  return(0);
+  return (0);
 }
 
-void procs_args(char **av, t_data da) {
+int procs_args(char **av, t_data *da) {
   da->nb_philo = ft_atoi(av[1]);
   da->time_die = ft_atoi(av[2]);
   da->time_eat = ft_atoi(av[3]);
   da->time_sleep = ft_atoi(av[4]);
   if (av[5])
-      da->ds->nb_goal = ft_atoi(av[5]);
+    da->nb_goal = ft_atoi(av[5]);
   da->ph = malloc(sizeof(da->ph) * da->nb_philo);
   if (!da->ph)
-    return(1);
+    return (1);
+  return (0);
 }
 
-int init_args(int ac, char **av, t_data da) {
-  
+int init_args(int ac, char **av, t_data *da) {
+
   if (ac < 5 || ac > 6)
-    return(1);
+    return (1);
   if (check_args(av))
-    return(1);
-  if (procs_args(av, ph))
-    return(1);
+    return (1);
+  if (procs_args(av, da))
+    return (1);
+  return (0);
 }
 
-
-
-int int main(int ac, char **av)
-{
+int main(int ac, char **av) {
   t_data da;
   if (init_args(ac, av, &da))
-      return(1);
+    return (1);
   // main loop
   // clean
 }
--- philo.h
+++ philo.h
@@ -1,21 +1,24 @@
 #ifndef PHILO_H
-# define PHILO_H
+#define PHILO_H
 
+#include <stdlib.h>
 
-
-typedef struct s_data
-{
+typedef struct s_data {
   int nb_philo;
   int time_die;
   int time_eat;
   int time_sleep;
   int nb_goal;
+  struct s_philo *ph;
 } t_data;
 
-typedef struct s_philo
-{
-  // id 
+typedef struct s_philo {
+  // id
   int nb_meals;
   int last_meals; // time last
   struct s_data *da;
 } t_philo;
+
+int ft_atoi(const char *nptr);
+
+#endif
