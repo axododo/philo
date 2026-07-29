81caddc007c0c7be064ca375de438699dbd74abc
axododo <guilbertmael@gmail.com>
2026-07-29 13:44:21 +0200

    start norm

--- main.c
+++ main.c
@@ -4,84 +4,95 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-long get_time(struct timeval start) {
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  long result =
-      (now.tv_sec - start.tv_sec) * 1000 + (now.tv_usec - start.tv_usec) / 1000;
-  return (result);
+long	get_time(struct timeval start)
+{
+	struct timeval	now;
+
+	gettimeofday(&now, NULL);
+	long result =
+		(now.tv_sec - start.tv_sec) * 1000 + (now.tv_usec - start.tv_usec)
+			/ 1000;
+	return (result);
 }
 
-int mprint(t_philo *ph, int mod) {
-  if (ph->rip == 1 && mod != 5)
-    return (0);
-  pthread_mutex_lock(&ph->da->print);
-  if (mod == 1)
-    printf("%li %i has taken a fork\n", get_time(ph->da->start), ph->id);
-  else if (mod == 2)
-    printf("%li %i is eating\n", get_time(ph->da->start), ph->id);
-  else if (mod == 3)
-    printf("%li %i is thinking\n", get_time(ph->da->start), ph->id);
-  else if (mod == 4)
-    printf("%li %i is sleeping\n", get_time(ph->da->start), ph->id);
-  else if (mod == 5)
-    printf("%li %i died\n", get_time(ph->da->start), ph->id);
-
-  pthread_mutex_unlock(&ph->da->print);
-  return (0);
+int	mprint(t_philo *ph, int mod)
+{
+	if (ph->rip == 1 && mod != 5)
+		return (0);
+	pthread_mutex_lock(&ph->da->print);
+	if (mod == 1)
+		printf("%li %i has taken a fork\n", get_time(ph->da->start), ph->id);
+	else if (mod == 2)
+		printf("%li %i is eating\n", get_time(ph->da->start), ph->id);
+	else if (mod == 3)
+		printf("%li %i is thinking\n", get_time(ph->da->start), ph->id);
+	else if (mod == 4)
+		printf("%li %i is sleeping\n", get_time(ph->da->start), ph->id);
+	else if (mod == 5)
+		printf("%li %i died\n", get_time(ph->da->start), ph->id);
+	pthread_mutex_unlock(&ph->da->print);
+	return (0);
 }
 
-int big_brother(t_philo *ph) {
-  long dif, now;
-  int i;
-
-  pthread_mutex_lock(&ph->da->stop);
-  i = 0;
-  while (i < ph->da->nb_philo) {
-    now = get_time(ph->da->start);
-    dif = now - ph->da->ph[i].last_meals;
-
-    if (dif > ph->da->time_die && ph->da->ph[i].nb_meals != ph->da->nb_goal) {
-      mprint(&ph->da->ph[i], 5);
-      ph->rip = 1;
-      break;
-    }
-    if (ph->da->ph[i].nb_meals == ph->da->nb_goal)
-      ph->nb_meals--;
-    i++;
-  }
-  if (ph->rip) {
-    i = 0;
-    while (i < ph->da->nb_philo) {
-      ph->da->ph[i].rip = 1;
-      i++;
-    }
-  }
-  pthread_mutex_unlock(&ph->da->stop);
-  return (1);
+int	big_brother(t_philo *ph)
+{
+	int	i;
+	long	dif; 
+	long	now;
+	
+	pthread_mutex_lock(&ph->da->stop);
+	i = 0;
+	while (i < ph->da->nb_philo)
+	{
+		now = get_time(ph->da->start);
+		dif = now - ph->da->ph[i].last_meals;
+		if (dif > ph->da->time_die && ph->da->ph[i].nb_meals != ph->da->nb_goal)
+		{
+			mprint(&ph->da->ph[i], 5);
+			ph->rip = 1;
+			break ;
+		}
+		if (ph->da->ph[i].nb_meals == ph->da->nb_goal)
+			ph->nb_meals--;
+		i++;
+	}
+	if (ph->rip)
+	{
+		i = 0;
+		while (i < ph->da->nb_philo)
+		{
+			ph->da->ph[i].rip = 1;
+			i++;
+		}
+	}
+	pthread_mutex_unlock(&ph->da->stop);
+	return (1);
 }
 
-void clean_all(t_data *da) {
-  int i;
+void	clean_all(t_data *da)
+{
+	int	i;
 
-  i = 0;
-  while (i < da->nb_philo + 1) {
-    pthread_mutex_destroy(&da->forks[i++]);
-  }
-  pthread_mutex_destroy(&da->print);
-  free(da->ph);
-  free(da->forks);
+	i = 0;
+	while (i < da->nb_philo + 1)
+	{
+		pthread_mutex_destroy(&da->forks[i++]);
+	}
+	pthread_mutex_destroy(&da->print);
+	free(da->ph);
+	free(da->forks);
 }
 
-int main(int ac, char **av) {
-  t_data da;
-  t_philo *ph;
+int	main(int ac, char **av)
+{
+	t_data	da;
+	t_philo	*ph;
 
-  if (init_args(ac, av, &da))
-    return (1);
-  init_philo(&da);
-  ph = da.ph;
-  summon_philo(ph, &da);
-  clean_all(&da);
-  return (0);
+	if (init_args(ac, av, &da))
+		return (1);
+	init_philo(&da);
+	ph = da.ph;
+	summon_philo(ph, &da);
+	clean_all(&da);
+	return (0);
 }
--- philo.c
+++ philo.c
@@ -3,34 +3,39 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int eating(t_philo *ph) {
-  int lwfork;
-  int hgfork;
+int	eating(t_philo *ph)
+{
+	int	lwfork;
+	int	hgfork;
 
-  if (ph->lFork < ph->rFork) {
-    lwfork = ph->lFork;
-    hgfork = ph->rFork;
-  } else {
-    lwfork = ph->rFork;
-    hgfork = ph->lFork;
-  }
-  pthread_mutex_lock(&ph->da->forks[lwfork]);
-  mprint(ph, 1);
-  if (ph->rip) {
-    pthread_mutex_unlock(&ph->da->forks[lwfork]);
-    return (0);
-  }
-  pthread_mutex_lock(&ph->da->forks[hgfork]);
-  mprint(ph, 1);
-  mprint(ph, 2);
-  pthread_mutex_lock(&ph->da->print);
-  ph->last_meals = get_time(ph->da->start);
-  ph->nb_meals++;
-  pthread_mutex_unlock(&ph->da->print);
-  usleep(ph->da->time_eat);
-  pthread_mutex_unlock(&ph->da->forks[hgfork]);
-  pthread_mutex_unlock(&ph->da->forks[lwfork]);
-  return (0);
+	if (ph->lFork < ph->rFork)
+	{
+		lwfork = ph->lFork;
+		hgfork = ph->rFork;
+	}
+	else
+	{
+		lwfork = ph->rFork;
+		hgfork = ph->lFork;
+	}
+	pthread_mutex_lock(&ph->da->forks[lwfork]);
+	mprint(ph, 1);
+	if (ph->rip)
+	{
+		pthread_mutex_unlock(&ph->da->forks[lwfork]);
+		return (0);
+	}
+	pthread_mutex_lock(&ph->da->forks[hgfork]);
+	mprint(ph, 1);
+	mprint(ph, 2);
+	pthread_mutex_lock(&ph->da->print);
+	ph->last_meals = get_time(ph->da->start);
+	ph->nb_meals++;
+	pthread_mutex_unlock(&ph->da->print);
+	usleep(ph->da->time_eat);
+	pthread_mutex_unlock(&ph->da->forks[hgfork]);
+	pthread_mutex_unlock(&ph->da->forks[lwfork]);
+	return (0);
 }
 
 /*int eating(t_philo *ph) {
@@ -66,61 +71,73 @@ int eating(t_philo *ph) {
   return (0);
 }*/
 
-int solo_eat(t_philo *ph) {
-  pthread_mutex_lock(&ph->da->forks[ph->lFork]);
-  mprint(ph, 1);
-  mprint(ph, 5);
-  ph->rip = 1;
-  pthread_mutex_unlock(&ph->da->forks[ph->lFork]);
-  return (0);
+int	solo_eat(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->da->forks[ph->lFork]);
+	mprint(ph, 1);
+	mprint(ph, 5);
+	ph->rip = 1;
+	pthread_mutex_unlock(&ph->da->forks[ph->lFork]);
+	return (0);
 }
 
-void *phi_loop(void *philo) {
-  t_philo *ph;
-  ph = philo;
-  if (ph->da->nb_philo == 1 && ph->id == 1) {
-    solo_eat(ph);
-    return (0);
-  }
-  if (ph->id % 2 == 0)
-    usleep(500);
-  if (ph->id >= 0 && ph->da->nb_philo > 1) {
-    while (1) {
-      if (ph->rip == 1)
-        break;
-      mprint(ph, 3);
-      usleep(125);
-      eating(ph);
-      if (ph->nb_meals == ph->da->nb_goal)
-        break;
-      if (ph->rip == 1)
-        break;
-      mprint(ph, 4);
-      usleep(ph->da->time_sleep);
-      if (ph->rip == 1)
-        break;
-    }
-  } else if (ph->id == -2 && ph->da->nb_philo > 1) {
-    while (ph->rip == 0 && ph->nb_meals > 0) {
-      big_brother(ph);
-      usleep(500);
-    }
-  }
-  return (0);
+void	*phi_loop(void *philo)
+{
+	t_philo	*ph;
+
+	ph = philo;
+	if (ph->da->nb_philo == 1 && ph->id == 1)
+	{
+		solo_eat(ph);
+		return (0);
+	}
+	if (ph->id % 2 == 0)
+		usleep(500);
+	if (ph->id >= 0 && ph->da->nb_philo > 1)
+	{
+		while (1)
+		{
+			if (ph->rip == 1)
+				break ;
+			mprint(ph, 3);
+			usleep(125);
+			eating(ph);
+			if (ph->nb_meals == ph->da->nb_goal)
+				break ;
+			if (ph->rip == 1)
+				break ;
+			mprint(ph, 4);
+			usleep(ph->da->time_sleep);
+			if (ph->rip == 1)
+				break ;
+		}
+	}
+	else if (ph->id == -2 && ph->da->nb_philo > 1)
+	{
+		while (ph->rip == 0 && ph->nb_meals > 0)
+		{
+			big_brother(ph);
+			usleep(500);
+		}
+	}
+	return (0);
 }
 
-int summon_philo(t_philo *ph, t_data *da) {
-  int i;
+int	summon_philo(t_philo *ph, t_data *da)
+{
+	int	i;
 
-  i = 0;
-  while (i < da->nb_philo + 1) {
-    pthread_create(&ph[i].stone, NULL, &phi_loop, &ph[i]);
-    i++;
-  }
-  i = 0;
-  while (i < da->nb_philo + 1) {
-    pthread_join(ph[i].stone, NULL);
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (i < da->nb_philo + 1)
+	{
+		pthread_create(&ph[i].stone, NULL, &phi_loop, &ph[i]);
+		i++;
+	}
+	i = 0;
+	while (i < da->nb_philo + 1)
+	{
+		pthread_join(ph[i].stone, NULL);
+		i++;
+	}
+	return (0);
 }
